362551325da1d89fe866dedaf31077d596d636dd
Sumit Kumar <summysumit100@gmail.com>
2022-10-18 18:18:01 +0530

    final commit

--- backend/settings.py
+++ backend/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # HEROKU STATIC FILES
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # cross origin middleware
     'django.middleware.common.CommonMiddleware',
